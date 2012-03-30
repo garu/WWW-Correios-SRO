6152772e7adb99be428507af485abccc8e604949
Breno G. de Oliveira <garu@cpan.org>
2012-03-30 06:12:04 -0300

    new version, with compliant Changes file

--- lib/WWW/Correios/SRO.pm
+++ lib/WWW/Correios/SRO.pm
@@ -22,7 +22,7 @@ use HTML::TreeBuilder;
 use parent 'Exporter';
 our @EXPORT_OK = qw( sro sro_en );
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 my $AGENT = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)';
 my $TIMEOUT = 30;
 
